8636f433a2df17f98f988b8f92fdf412fa4334ec
Pavel <dvdtygc@gmail.com>
2017-10-14 03:49:43 +0300

    Parameters update & DB Backup

---
+++ Database.sql
@@ -0,0 +1,308 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.18
+-- https://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 14, 2017 at 03:44 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `mysp5kth_mysport-bg`
+--
+
+--
+-- Dumping data for table `articles`
+--
+
+INSERT INTO `articles` (`id`, `posted_by_id`, `title`, `views`, `posted_on`, `image`, `content`, `deleted`) VALUES
+(1, 1, 'MEET THE 2016 USA OLYMPIC SWIMMING TEAM', 711, '2017-06-13', 'images/articles/13-06-2017-116512bd43d9caa6e02c990b0a82652dca594006a7390cd.png', 'I have become a habitual Instagrammer.\r\n\r\nWhen I am out cycling, walking or just sitting admiring the view or the passing parade, I am looking for photo oportunities that I can snap and then share on my Instagram “app”.\r\n\r\n6 Powerful Reasons Why you Should include Images in your Marketing - Infographic\r\n\r\nAll I need to be carrying is my iPhone or iPad and with my Instagram app waiting patiently for me, I can capture that photo, apply a snazzy filter and click the Facebook and the Twitter share buttons and it flies around the world.\r\n\r\nIt then showcases in my follower’s streams and updates and invites them to engage with me by commenting, liking or sharing.\r\n\r\nJamie Oliver a “celebrity chef”, understands this and with over 412,000 followers on Instagram has  embraced the power of photos to market the Jamie Oliver brand.\r\n\r\nNot surprisingly, the better the photo the more it drives engagement and Instagram takes my ordinary and average photos and gives them that “zing” factor (a good example is my Instagram photo from Istanbul, Turkey in the top right corner of this post) .\r\n\r\nThe Age of Visual Culture\r\n\r\nWe live in the age of the “camera in everyone’s pocket” and with more than 2.5 billion camera phones in use we are entering a new dynamic era around image creation and content according to Bob Lisbon from Luminate.\r\n\r\nThis can be broken into three phases as we enter the age of visual culture and language.\r\n\r\nPhase One: Massive increase in photo creation\r\n\r\nTen percent of photos taken by humankind took place in the last 12 months\r\n\r\nPhase Two: The Rise of Image Centric Social Networks', 0),
+(2, 1, 'MEET THE 2016 USA OLYMPIC SWIMMING TEAM', 16, '2017-06-13', 'images/articles/13-06-2017-512838023a778dfaecdc212708f721b78859401be727bbe.png', 'I have become a habitual Instagrammer. When I am out cycling, walking or just sitting admiring the view or the passing parade, I am looking for photo oportunities that I can snap and then share on my Instagram “app”. 6 Powerful Reasons Why you Should include Images in your Marketing - Infographic All I need to be carrying is my iPhone or iPad and with my Instagram app waiting patiently for me, I can capture that photo, apply a snazzy filter and click the Facebook and the Twitter share buttons and it flies around the world. It then showcases in my follower’s streams and updates and invites them to engage with me by commenting, liking or sharing. Jamie Oliver a “celebrity chef”, understands this and with over 412,000 followers on Instagram has embraced the power of photos to market the Jamie Oliver brand. Not surprisingly, the better the photo the more it drives engagement and Instagram takes my ordinary and average photos and gives them that “zing” factor (a good example is my Instagram photo from Istanbul, Turkey in the top right corner of this post) . The Age of Visual Culture We live in the age of the “camera in everyone’s pocket” and with more than 2.5 billion camera phones in use we are entering a new dynamic era around image creation and content according to Bob Lisbon from Luminate. This can be broken into three phases as we enter the age of visual culture and language. Phase One: Massive increase in photo creation Ten percent of photos taken by humankind took place in the last 12 months Phase Two: The Rise of Image Centric Social Networks', 0),
+(3, 1, 'MEET THE 2016 USA OLYMPIC SWIMMING TEAM', 16, '2017-06-13', 'images/articles/13-06-2017-090a8005f5594bd67041f88c619619264659401bf9e1993.png', 'I have become a habitual Instagrammer. When I am out cycling, walking or just sitting admiring the view or the passing parade, I am looking for photo oportunities that I can snap and then share on my Instagram “app”. 6 Powerful Reasons Why you Should include Images in your Marketing - Infographic All I need to be carrying is my iPhone or iPad and with my Instagram app waiting patiently for me, I can capture that photo, apply a snazzy filter and click the Facebook and the Twitter share buttons and it flies around the world. It then showcases in my follower’s streams and updates and invites them to engage with me by commenting, liking or sharing. Jamie Oliver a “celebrity chef”, understands this and with over 412,000 followers on Instagram has embraced the power of photos to market the Jamie Oliver brand. Not surprisingly, the better the photo the more it drives engagement and Instagram takes my ordinary and average photos and gives them that “zing” factor (a good example is my Instagram photo from Istanbul, Turkey in the top right corner of this post) . The Age of Visual Culture We live in the age of the “camera in everyone’s pocket” and with more than 2.5 billion camera phones in use we are entering a new dynamic era around image creation and content according to Bob Lisbon from Luminate. This can be broken into three phases as we enter the age of visual culture and language. Phase One: Massive increase in photo creation Ten percent of photos taken by humankind took place in the last 12 months Phase Two: The Rise of Image Centric Social Networks', 0),
+(4, 1, 'MEET THE 2016 USA OLYMPIC SWIMMING TEAM', 15, '2017-06-13', 'images/articles/13-06-2017-34e369853df766fa44e1ed0ff613f563bd59401c4e6973a.png', 'I have become a habitual Instagrammer. When I am out cycling, walking or just sitting admiring the view or the passing parade, I am looking for photo oportunities that I can snap and then share on my Instagram “app”. 6 Powerful Reasons Why you Should include Images in your Marketing - Infographic All I need to be carrying is my iPhone or iPad and with my Instagram app waiting patiently for me, I can capture that photo, apply a snazzy filter and click the Facebook and the Twitter share buttons and it flies around the world. It then showcases in my follower’s streams and updates and invites them to engage with me by commenting, liking or sharing. Jamie Oliver a “celebrity chef”, understands this and with over 412,000 followers on Instagram has embraced the power of photos to market the Jamie Oliver brand. Not surprisingly, the better the photo the more it drives engagement and Instagram takes my ordinary and average photos and gives them that “zing” factor (a good example is my Instagram photo from Istanbul, Turkey in the top right corner of this post) . The Age of Visual Culture We live in the age of the “camera in everyone’s pocket” and with more than 2.5 billion camera phones in use we are entering a new dynamic era around image creation and content according to Bob Lisbon from Luminate. This can be broken into three phases as we enter the age of visual culture and language. Phase One: Massive increase in photo creation Ten percent of photos taken by humankind took place in the last 12 months Phase Two: The Rise of Image Centric Social Networks', 0);
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`id`, `name`) VALUES
+(1, 'Mares'),
+(2, 'Head');
+
+--
+-- Dumping data for table `carts`
+--
+
+INSERT INTO `carts` (`id`, `owner_id`, `product_id`, `quantity`, `color_id`, `size_id`, `status_id`, `posted_on`) VALUES
+(1, 1, 2, 5, 1, 9, 1, '2017-08-07 14:19:44');
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(5, 'Accessories'),
+(6, 'Footwear'),
+(4, 'Goggles'),
+(3, 'Neoprene'),
+(1, 'Swimsuit'),
+(2, 'Teamline');
+
+--
+-- Dumping data for table `colors`
+--
+
+INSERT INTO `colors` (`id`, `name`, `name_bg`, `hex`) VALUES
+(1, 'Black-Black', 'Черен-Черен', '#000000'),
+(2, 'Green', 'Зелен', '#11ad23'),
+(3, 'Light Blue', 'Светлосин', '#1eedce'),
+(4, 'Royal', 'Роял', '#4169e1'),
+(5, 'Black-Silver', 'Черно-Сребърно', '#c0c0c0'),
+(6, 'Lime', 'Лайм', '#99ff00'),
+(7, 'Red', 'Червен', '#ff0000'),
+(8, 'Germany (black, red-yellow)', 'Германия (черно, червено-жълто)', '#FCA218'),
+(9, 'Black', 'Черен', '#000000'),
+(10, 'Black-orange', 'Черно-оранжево', '#CB5D3D'),
+(11, 'Brasil (yellow-green)', 'Бразилия (жълто-зелено)', '#039242'),
+(12, 'Blue', 'Син', '#0000ff');
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `serialNumber`, `price`, `image`, `description`, `live`, `addedOn`, `addedById`, `brand_id`, `secondary_category_id`) VALUES
+(1, 'SWS LIQUIDFIRE POWER - TANK', 452101, '17.50', 'images/products/11-07-2017-4767c6a1e7ce56d3d6fa748ab6d9af3fd759642b73a8286.png', NULL, 1, '2017-07-07 02:46:53', 1, 1, 1),
+(2, 'SWS LIQUIDFIRE POWER KNEE-OPEN BACK LADY', 452102, '28.00', 'images/products/11-07-2017-241ff1de774005f8da13f42943881c655f596429049397f.png', NULL, 1, '2017-07-07 03:44:47', 1, 1, 1),
+(3, 'SWS LIQUIDFIRE KNEE PRO OPEN BACK', 452203, '132.00', 'images/products/11-07-2017-5772b32a1f754ba1c09b3695e0cb6cde7f596429258c432.png', NULL, 1, '2017-07-10 21:15:16', 1, 1, 1),
+(4, 'SWS LIQUIDFIRE PRO KNEE CLOSE BACK', 452211, '0.00', 'images/products/11-07-2017-116512bd43d9caa6e02c990b0a82652dca59642933d486b.png', NULL, 1, '2017-07-10 22:09:23', 1, 1, 1),
+(5, 'SWS TEAM LIQUIDPOWER KNEE LADY', 452252, '0.00', 'images/products/10-07-2017-159bf31c7ff062936a96d3c8bd1f8f2ff35963d153dddf2.png', NULL, 1, '2017-07-10 22:11:15', 1, 1, 1),
+(6, 'SWS TEAM LIQUIDPOWER TANK LADY', 452253, '0.00', 'images/products/11-07-2017-33182be0c5cdcd5072bb1864cdee4d3d6e59642d4541f19.png', NULL, 1, '2017-07-11 04:43:13', 1, 1, 1),
+(7, 'SWS LIQUIDFIRE KNEE VECTOR OPEN BACK LAD', 452270, '125.00', 'images/products/11-07-2017-54a684eceee76fc522773286a895bc843659642fee41bae.png', NULL, 1, '2017-07-11 04:50:54', 1, 1, 1),
+(8, 'SWS LIQUID FIRE KNEE VX OPEN BACK', 452327, '130.00', 'images/products/11-07-2017-38a5771bce93e200c36f7cd9dfd0e5deaa596432ea18776.png', NULL, 1, '2017-07-11 05:07:31', 1, 1, 1),
+(9, 'SWS LIQUID FIRE KNEE VX CLOSE BACK', 452328, '130.00', 'images/products/11-07-2017-1770efdf2ec9b086079795c442636b55fb596434f1cf9aa.png', NULL, 1, '2017-07-11 05:15:57', 1, 1, 1),
+(10, 'SWS LF CORE KNEE CLOSE BACK LADY', 452362, '0.00', 'images/products/11-07-2017-2337693cfc748049e45d87b8c7d8b9aacd59643b4b7492c.png', NULL, 1, '2017-07-11 05:43:16', 1, 1, 1);
+
+--
+-- Dumping data for table `products_colors`
+--
+
+INSERT INTO `products_colors` (`product_id`, `color_id`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(1, 6),
+(2, 1),
+(2, 2),
+(2, 3),
+(2, 4),
+(2, 5),
+(2, 6),
+(2, 7),
+(3, 8),
+(3, 9),
+(4, 9),
+(5, 9),
+(6, 9),
+(7, 9),
+(7, 10),
+(8, 4),
+(8, 11),
+(9, 4),
+(9, 11),
+(10, 12);
+
+--
+-- Dumping data for table `products_sizes`
+--
+
+INSERT INTO `products_sizes` (`product_id`, `size_id`) VALUES
+(1, 3),
+(1, 4),
+(1, 5),
+(1, 6),
+(1, 7),
+(1, 8),
+(1, 9),
+(2, 1),
+(2, 2),
+(2, 3),
+(2, 4),
+(2, 5),
+(2, 6),
+(2, 7),
+(2, 8),
+(2, 9),
+(3, 3),
+(3, 4),
+(3, 5),
+(3, 6),
+(3, 7),
+(3, 8),
+(3, 9),
+(4, 3),
+(4, 4),
+(4, 5),
+(4, 6),
+(4, 7),
+(4, 8),
+(4, 9),
+(5, 3),
+(5, 4),
+(5, 5),
+(5, 6),
+(5, 7),
+(5, 8),
+(5, 9),
+(6, 3),
+(6, 4),
+(6, 5),
+(6, 6),
+(6, 7),
+(6, 8),
+(6, 9),
+(7, 3),
+(7, 4),
+(7, 5),
+(7, 6),
+(7, 7),
+(7, 8),
+(7, 9),
+(7, 10),
+(7, 11),
+(8, 1),
+(8, 2),
+(8, 3),
+(8, 4),
+(8, 5),
+(8, 6),
+(8, 7),
+(8, 8),
+(8, 9),
+(9, 1),
+(9, 2),
+(9, 3),
+(9, 4),
+(9, 5),
+(9, 6),
+(9, 7),
+(9, 8),
+(10, 2),
+(10, 4),
+(10, 5),
+(10, 6),
+(10, 7),
+(10, 8);
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`) VALUES
+(1, 'ROLE_USER'),
+(2, 'ROLE_ADMIN');
+
+--
+-- Dumping data for table `secondary_categories`
+--
+
+INSERT INTO `secondary_categories` (`id`, `name`, `category_id`) VALUES
+(1, 'Racing (woman)', 1),
+(2, 'Openwater (woman)', 1),
+(3, 'Training (women)', 1),
+(4, 'Aquafit (women)', 1),
+(5, 'Racing (men)', 1),
+(6, 'Openwater (men)', 1),
+(7, 'Training (men)', 1),
+(8, 'Watershorts (men)', 1),
+(9, 'Training (girl)', 1),
+(10, 'Racing (boy)', 1),
+(11, 'Training (boy)', 1),
+(12, 'Kids 0-2', 1),
+(13, 'Man', 2),
+(14, 'Lady', 2),
+(15, 'Unisex', 2),
+(16, 'Boy & girl', 2),
+(17, 'Wetsuit swimrun', 3),
+(18, 'Open water & outdoor', 3),
+(19, 'Tria stretch gear', 3),
+(20, 'Tria accessories', 3),
+(21, 'Neo & swimrun', 3),
+(22, 'Racing', 4),
+(23, 'Performance', 4),
+(24, 'Optical', 4),
+(25, 'Waterstorts', 4),
+(26, 'Junior', 4),
+(27, 'Infaltable equipment', 5),
+(28, 'Caps', 5),
+(29, 'Bags', 5),
+(30, 'Waterpolo', 5),
+(31, 'Adult', 6),
+(32, 'Junior', 6);
+
+--
+-- Dumping data for table `sizes`
+--
+
+INSERT INTO `sizes` (`id`, `name`) VALUES
+(1, '30'),
+(2, '32'),
+(3, '34'),
+(4, '36'),
+(5, '38'),
+(6, '40'),
+(7, '42'),
+(8, '44'),
+(9, '46'),
+(10, '48'),
+(11, '50');
+
+--
+-- Dumping data for table `statuses`
+--
+
+INSERT INTO `statuses` (`id`, `name`, `name_bg`) VALUES
+(1, 'Waiting for payment', 'Изчакване за плащане'),
+(2, 'Waiting for authorization', 'Изчакване за одобрение'),
+(3, 'Shipment processing', 'Обработка на доставка'),
+(4, 'Shipping to address', 'Доставяне към адрес'),
+(5, 'Finished', 'Приключена');
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `firstName`, `lastName`, `discount`, `country`, `city`, `zip`, `phone`, `address`) VALUES
+(1, 'pavel@abv.bg', '$2y$13$U1mVF0xTM0RG44h.bzSFxO8TU3TTSlNJDbXCxaC2z68Wj17dwPevy', 'Pavel', 'Grancharov', 100, 'Bulgaria', 'Varna', 9000, 87123, 'Hadji Dimitar 34234'),
+(2, 'kgod@gmail.com', '$2y$13$QEwOpdWVF/lTlRTqPwp3J.qs1zYBdHz3IW2PCJzCrTkXr8yavdfqm', 'Kolento', 'XHope', 0, NULL, NULL, NULL, NULL, NULL),
+(3, 'testt@gmail.com', '$2y$13$FHMKaqZTRxS/.fZ4r0Bt5O7lizNc3UWoNm7Y4DwU6IYaxQlsx8E7O', 'Ivan', 'Draganov', 0, NULL, NULL, NULL, NULL, NULL),
+(4, 'jukolen1818@gmail.com', '$2y$13$U7ZniAHrWfOF7XihlHHXPOULz6TwjG.sqj0oTRVBDpdNz7F3KAOgO', 'Jenq', 'Petrovskaq', 0, NULL, NULL, NULL, NULL, NULL),
+(5, 'juk1995@mail.com', '$2y$13$xWFoay4u5Api8Yh0MgCHRO2tUXAhhz7eA7uBf1dWyHNWPWbMeOAtG', 'Evgeniq', 'Grancharova', 0, NULL, NULL, NULL, NULL, NULL),
+(6, 'emailed@abv.bg', '$2y$13$7Bbjq3TllEHi5iex654CW.MC3upFWp309h9LHfg3y8BxXjqmRKTtq', '321', '123', 0, NULL, NULL, NULL, NULL, NULL);
+
+--
+-- Dumping data for table `users_roles`
+--
+
+INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES
+(1, 1),
+(1, 2),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 1),
+(6, 1);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
